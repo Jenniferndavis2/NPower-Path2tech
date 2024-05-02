9ef5757976d428a1a54bf8dcbdb0556156e172ba
Jenniferndavis2 <146387542+Jenniferndavis2@users.noreply.github.com>
2024-05-02 12:23:41 -0500

    first commit

---
+++ Path 2 Tech/JAVA SCRIPT/JS Lessons/lesson01/lesson01.js
@@ -0,0 +1,29 @@
+// # 1. Adjust the age and name variables to your own age and name, and then fill in the 
+console.log statements to complete the code.
+let age = 0;
+let name_one = "";
+// # Print out the variables below
+console.log("My age is: ", ____);
+console.log("My name is: ", _____);
+// # 2. Reassign the value of x to 20 and then print out the value of x
+let x = 0;
+// # Reassign the value of x to 20 below
+// # Print out the value of x below
+console.log("The value of x is: ", ___);
+// # 3. Below is a constant variable of PI assigned to 3.14. Try to reassign the value of 
+PI below, are you able to?
+const PI = 3.14;
+// # Try to reassign the value of PI below, are you able to?
+// # Replace this comment with your code
+// # Create a constant variable called my_name and assign it to your name. Then print both 
+PI and my_name to the console.
+// # Replace this comment with your code
+console.log("The value of PI is: ", ___);
+console.log("My name is: ", ___);
+// # 4. Below are some different data types of variables. Try to print out the data types 
+of each variable below. Hint: use the typeof operator.
+let myNum = 15;
+let myString = "Hello, World!";
+// # Print out the data types of each variable below
+console.log(typeof ___);
+console.log(typeof ___)
